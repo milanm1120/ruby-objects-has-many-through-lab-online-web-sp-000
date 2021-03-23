5e700eea2eebf9e08277fb2eba365526a8314343
Milan Mody <milanm1120@gmail.com>
2021-03-23 02:26:09 +0000

    Automatically backed up by Learn

---
+++ lib/doctor.rb
@@ -0,0 +1,27 @@
+class Doctor
+   attr_accessor :name
+
+   @@all = []
+
+   def initialize(name)
+     @name = name
+     @@all << self
+   end
+
+   def self.all
+     @@all
+   end
+
+   def new_appointment(date, patient)
+     Appointment.new(date, patient, doctor)
+   end
+
+   def appointments
+     Appointment.all.select { |app| app.doctor == self }
+   end
+
+   def patients
+     appointments.map { |app| app.patient }
+   end
+
+ end 
