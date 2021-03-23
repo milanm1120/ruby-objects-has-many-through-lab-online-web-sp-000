77c7dfccd14f387f9cdcab6227ddaed71320c8ca
Milan Mody <milanm1120@gmail.com>
2021-03-23 02:24:52 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -13,12 +13,12 @@ class Doctor
     @@all
   end
 
-  def new_appointment(date, patient)
-    Appointment.new(date, patient, self)
-  end
-
   def appointments
     Appointment.all.select { |appointment| appointment.doctor == self }
+  end
+
+  def new_appointment(date, patient)
+    Appointment.new(date, patient, self)
   end
 
   def patients
