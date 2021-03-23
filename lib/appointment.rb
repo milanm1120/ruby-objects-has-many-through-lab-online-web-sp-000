6cccd01b5f97b3c4894264a2108e0dbf8d2e67b8
Milan Mody <milanm1120@gmail.com>
2021-03-23 02:30:29 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,17 +1,18 @@
 class Appointment
-   attr_accessor :date, :doctor, :patient
 
-   @@all = []
+  attr_accessor :date, :doctor, :patient
 
-   def initialize(date, patient, doctor)
-     @date = date
-     @patient = patient
-     @doctor = doctor
-     @@all << self
-   end
+  @@all = []
 
-   def self.all
-     @@all
-   end
-   
- end
+  def initialize(date, patient, doctor)
+    @date = date
+    @patient = patient
+    @doctor = doctor
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+end
